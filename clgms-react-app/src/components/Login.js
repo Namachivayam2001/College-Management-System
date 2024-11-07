389e0ec23b19321342cb2fd1ce98dd0cf8cd7a73
Namachivayam2001 <namachivayam10419u25030@gmail.com>
2024-11-07 10:09:46 +0530

    Update login page and packages

--- clgms-react-app/src/components/Login.js
+++ clgms-react-app/src/components/Login.js
@@ -1,39 +1,92 @@
-import React from 'react';
+import React, { useState } from 'react';
+// import { useDispatch } from 'react-redux';
 import { Container, Row, Col, Card, Form, Button } from 'react-bootstrap';
+import axios from 'axios';
+// import { loginUser } from '../redux/actions'; // Assuming a Redux action is set up
 
 function Login() {
-  return (
-    <Container fluid className="d-flex justify-content-center align-items-center vh-100">
-      <Row className="d-flex justify-content-center align-items-center h-100">
-        <Col xs={10} sm={10} md={10} lg={10} xl={10}> {/* Adjusts width based on screen size */}
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
+    // const dispatch = useDispatch();
 
-          <Card className="bg-dark text-white my-5 mx-auto" style={{ borderRadius: '1rem' }}>
-            <Card.Body className="p-5 d-flex flex-column align-items-center mx-auto w-100">
+    const handleLogin = async (e) => {
+        e.preventDefault();
+        setErrors({}); // Clear previous errors
 
-              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
-              <p className="text-white-50 mb-5">Please enter your login and password!</p>
+        try {
+            const response = await axios.post('/user/login/', { username, password });
+            const userData = response.data;
+            console.log(userData)
+            // dispatch(loginUser(userData)); // Store user data in Redux
+        } catch (error) {
+            if (error.response && error.response.data) {
+                setErrors(error.response.data.errors || { general: 'An error occurred' });
+            } else {
+                setErrors({ general: 'An error occurred' });
+            }
+        }
+    };
 
-              <Form.Group className="mb-4 mx-5 w-100">
-                <Form.Label className="text-white">Email address</Form.Label>
-                <Form.Control type="email" size="lg" />
-              </Form.Group>
+    return (
+        <Container fluid className="d-flex justify-content-center align-items-center vh-100">
+        <Row className="d-flex justify-content-center align-items-center w-100 h-90">
+            <Col md={6} lg={5} xl={5}>
 
-              <Form.Group className="mb-4 mx-5 w-100">
-                <Form.Label className="text-white">Password</Form.Label>
-                <Form.Control type="password" size="lg" />
-              </Form.Group>
+            <Card className="bg-dark text-white my-5" style={{ borderRadius: '1rem' }}>
+                <Card.Body className="p-5 d-flex flex-column align-items-center">
 
-              <p className="small mb-3 pb-lg-2"><a href="#!" className="text-white-50">Forgot password?</a></p>
-              
-              <Button variant="outline-light" className="mx-2 px-5" size="lg">Login</Button>
+                <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+                <p className="text-white-50 mb-5 text-center">Please enter your login and password!</p>
 
-            </Card.Body>
-          </Card>
+                <Form onSubmit={handleLogin} className="d-flex flex-column align-items-center">
 
-        </Col>
-      </Row>
+                    <Form.Group className="mb-4" >
+                    <Form.Label className="text-white">User Name</Form.Label>
+                    <Form.Control
+                        type="username"
+                        size="lg"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        isInvalid={!!errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.email}
+                    </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group className="mb-4">
+                    <Form.Label className="text-white">Password</Form.Label>
+                    <Form.Control
+                        type="password"
+                        size="lg"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        isInvalid={!!errors.password}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                        {errors.password}
+                    </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <p className="small mx-auto">
+                    <a href="#!" className="text-white-50">Forgot password?</a>
+                    </p>
+                    
+                    <Button variant="outline-light" className="px-5" size="lg" type="submit">Login</Button>
+
+                    {errors.general && (
+                    <div className="text-danger mt-3">{errors.general}</div>
+                    )}
+
+                </Form>
+                </Card.Body>
+            </Card>
+
+            </Col>
+        </Row>
     </Container>
-  );
+    );
 }
 
 export default Login;
